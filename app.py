140aecd486d8efe45518af09b3b780f09951767f
Whythree <whythree1.dollars@gmail.com>
2025-06-24 11:15:52 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from weasyprint import HTML
+import flask
+import os
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__)
+
+@app.route("/")
+#Von Home muss eine JSON an /create-invoice gesendet werden mit einer Form.
+def home():
+    pass
+
+
+@app.post("/create-invoice")
+def create_invoice():
+    today = datetime.today().strftime("%B %-d, %Y")
+    rendered_template = render_template('invoice.html',
+                                        date=today)
+
+    html = HTML(string=rendered_template)
+    rendered_pdf = html.write_pdf('./static/invoice.pdf')
+    return send_file("./static/invoice.pdf")
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+#
+# html = HTML('invoice.html')
+# html.write_pdf('invoice.pdf')
